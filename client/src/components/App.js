086e7222b1b892f28b07fb794be1e7450939d1de
cjstanzhu <cjstanzhu@gmail.com>
2020-03-18 23:51:02 -0400

    rearrange routes in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,8 +17,8 @@ const App = () => {
 
                     <Switch>
                         <Route path="/" exact component={StreamList} />
-                        <Route path="/streams/:id" exact component={StreamShow} />
                         <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/:id" exact component={StreamShow} />
                         <Route path="/streams/edit/:id" exact component={StreamEdit} />
                         <Route path="/streams/delete/:id" exact component={StreamDelete} />
                     </Switch>
